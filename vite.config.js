64d464fff1e713b2e409ec4810e03bd34fae5d7f
Aman Kumar <aman9995199@gmail.com>
2026-05-15 09:11:40 -0400

    added

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://trade-vault-back-end-new-v1.vercel.app/',
-        changeOrigin: true,
-      },
-      '/ws': {
-        target: 'wss://trade-vault-back-end-new-v1.vercel.app/',
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL + '/',
+          changeOrigin: true,
+        },
+        '/ws': {
+          target: 'wss://' + env.VITE_API_URL.replace('https://', '') + '/',
+          ws: true,
+        }
       }
     }
   }
